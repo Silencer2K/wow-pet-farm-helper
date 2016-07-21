55b96fea51e36e4a8e6b4c19a3d37930f361761a
Silencer <silencer2k@gmail.com>
2016-07-21 19:18:59 +0000

    tooltip position fix

--- PetFarmHelper.lua
+++ PetFarmHelper.lua
@@ -159,7 +159,7 @@ function addon:OnCombatEvent(event, timeStamp, logEvent, hideCaster,
 end
 
 function addon:UpdateTooltip(anchor)
-    if not InCombatLockdown() then
+    if not InCombatLockdown() and not (self.tooltip and self.tooltip:IsShown()) then
         if qtip:IsAcquired('PetFarmHelper') and self.tooltip then
             self.tooltip:Clear()
         else
